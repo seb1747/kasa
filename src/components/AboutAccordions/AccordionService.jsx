bb06d0bb7d7ffbd025f2853384f115c922b0764b
sebastien binet <sebastienbinet47@gmail.com>
2022-10-12 19:14:07 +0200

    fix orthographe

--- src/components/AboutAccordions/AccordionService.jsx
+++ src/components/AboutAccordions/AccordionService.jsx
@@ -14,7 +14,7 @@ export default function AccordionFiability() {
               Services <i className="fa-solid fa-chevron-down"></i>
           </summary>
           <p className="about__info__text">
-            Nos équipes se tiennent à votre disposition pour vous fournir ine expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.
+            Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.
           </p>
       </details>
     </div>
